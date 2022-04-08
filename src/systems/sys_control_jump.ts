3688285573e13c14bfef2829a550c71af9f582e7
Stanisław Małolepszy <sta@malolepszy.org>
2022-04-08 15:32:35 +0200

    Jump forward a bit, too

--- src/systems/sys_control_jump.ts
+++ src/systems/sys_control_jump.ts
@@ -1,10 +1,12 @@
+import {Vec3} from "../../common/math.js";
+import {normalize, scale, transform_direction} from "../../common/vec3.js";
 import {Entity} from "../../common/world.js";
 import {query_down} from "../components/com_children.js";
 import {Game} from "../game.js";
 import {snd_jump} from "../sounds/snd_jump.js";
 import {Has} from "../world.js";
 
-const QUERY = Has.ControlPlayer | Has.RigidBody;
+const QUERY = Has.ControlPlayer | Has.RigidBody | Has.Transform;
 
 export function sys_control_jump(game: Game, delta: number) {
     for (let i = 0; i < game.World.Signature.length; i++) {
@@ -14,9 +16,15 @@ export function sys_control_jump(game: Game, delta: number) {
     }
 }
 
+const jump_magnitude = 180;
+const local_direction_title: Vec3 = [0, 1, 0];
+const local_direction_playing: Vec3 = [0, 1, 0.2];
+const world_direction: Vec3 = [0, 0, 0];
+
 function update(game: Game, entity: Entity) {
     let control = game.World.ControlPlayer[entity];
     let rigid_body = game.World.RigidBody[entity];
+    let transform = game.World.Transform[entity];
 
     if (control.Jump) {
         if (
@@ -25,7 +33,14 @@ function update(game: Game, entity: Entity) {
         ) {
             // Jump
             if (!rigid_body.IsAirborne) {
-                rigid_body.Acceleration[1] += 200;
+                // Acceleration must be set in world-space.
+                if (game.PlayState === "playing") {
+                    transform_direction(world_direction, local_direction_playing, transform.World);
+                } else {
+                    transform_direction(world_direction, local_direction_title, transform.World);
+                }
+                normalize(world_direction, world_direction);
+                scale(rigid_body.Acceleration, world_direction, jump_magnitude);
 
                 for (let ent of query_down(game.World, entity, Has.Animate)) {
                     game.World.Animate[ent].Trigger = "jump";
